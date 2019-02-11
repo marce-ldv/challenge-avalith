909d39d07e40b8be92b090860272a1e39b020a93
marcelo-docutec <marcelo.docutec@gmail.com>
2019-02-11 13:18:39 -0300

    add image to background (parallax effect) and fixes

--- src/components/Main/Content/PostContent/PostContent.js
+++ src/components/Main/Content/PostContent/PostContent.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import './PostContent.scss';
 import serviceFetchData from '../../../../services/serviceFetchData';
+//var DoughnutChart = require("react-chartjs").Doughnut; //ES5
+//import { Doughnut } from 'react-chart.js'; //ES6
 
 export default class PostContent extends Component{
 
@@ -22,8 +24,14 @@ export default class PostContent extends Component{
     render(){
         return(
             <div>
-                <h2>helllo i'm post</h2>
+                <div className="jumbotron img-jumbotron">
+                    {/*<img src={this.state.serviceData.cardPost.postImageUrl} alt="dev"/>*/}
+                    <img src="https://dummyimage.com/1366x768/000/fff" alt="dev"/>
+                </div>
                 {/*<h2>{this.state.serviceData.cardPost.postTitle}</h2>*/}
+                {/*Pie Chart*/}
+                {/*<canvas id="pieChart" width="400" height="400"></canvas>*/}
+
             </div>
         );
     }
